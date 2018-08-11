42e6e1b7ab6633873bc2598736e9c9418c0c45ce
Raj Pandey <rk.pandey@hotmail.com>
2018-08-11 22:36:27 +0530

    Greet new user

--- Server/server.js
+++ Server/server.js
@@ -13,7 +13,16 @@ app.use(express.static(publicpath));
 
 io.on('connection',(socket)=>{
     console.log('New user connected');
-
+    socket.emit('newMessage',{
+        from:'Admin',
+        text:'Welcome to chat app',
+        createdAt:new Date().getTime()
+    });
+    socket.broadcast.emit('newMessage',{
+        from:'Admin',
+        text:'New user joined',
+        createdAt:new Date().getTime()
+    });
     socket.on('createMessage',(message)=>{
         console.log('createMessage', message);
         io.emit('newMessage',{
@@ -21,6 +30,11 @@ io.on('connection',(socket)=>{
             text:message.text,
             createdAt:new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage',{
+        //     from:message.from,
+        //     text:message.text,
+        //     createdAt:new Date().getTime()
+        // });
      });
     socket.on('disconnect', ()=>{
         console.log('User was disconnected');
