1b2522955c21485901926d7f6325521967ab3440
Raj Pandey <rk.pandey@hotmail.com>
2018-08-12 18:25:46 +0530

    test

--- public/js/index.js
+++ public/js/index.js
@@ -14,10 +14,10 @@ socket.on('newMessage', function (message) {
 });
 
 socket.on('newLocationMessage', function (message) {
-    console.log('newMessage', message);
-    console.log(message.url);
+    console.log('newLocationMessage', message);
+    console.log(message.from);
     // var li = $(`<li><a target='_blank' href=${message.url}>My current Location</a></li>`);
-    var li = $(`<li>${message.url}</li>`);
+    var li = $(`<li>${message.from}</li>`);
     $('#messages').append(li);
   });
   
